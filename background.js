0adde93a3754c5d0c6ca3ff36d444c7ac3b00ae9
adenita <nittaluta@gmail.com>
2023-12-29 05:24:17 +0100

    add event listener on active tabs and store necessary data

---
+++ background.js
@@ -0,0 +1,22 @@
+// Initialize tabData object to track active tabs
+let tabData = {};
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    const tabId = activeInfo.tabId;
+    const currentTime = new Date().getTime();
+
+    chrome.tabs.get(tabId, (tab) => {
+        const url = tab.url;
+        if (!tabData[tabId]) {
+            tabData[tabId] = {
+                startTime: currentTime,
+                totalTime: 0,
+                url: url
+            };
+        } else {
+            const elapsedTime = currentTime - tabData[tabId].startTime;
+            tabData[tabId].totalTime += elapsedTime;
+            tabData[tabId].startTime = currentTime;
+        }
+    })
+});
