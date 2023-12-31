c9e05e60449d4930f0282e86427d048b3b84fb35
adenita <nittaluta@gmail.com>
2023-12-31 04:04:30 +0100

    refactored update popup function into smaller functions for better readability

--- popup/timer-popup.js
+++ popup/timer-popup.js
@@ -9,85 +9,104 @@ document.addEventListener('DOMContentLoaded', function () {
 function updatePopup(response) {
     const tabsListContainer = document.getElementById('tabs-list-container');
     const noTabsMessage = document.getElementById('no-tabs-message');
-
     tabsListContainer.innerHTML = '';
 
     // Check if the response object is empty (no tabs)
     if (Object.keys(response).length === 0) {
-        // display the no tab message
-        noTabsMessage.style.display = 'block';
-        tabsListContainer.style.display = 'none';
+        displayNoTabsMessage(noTabsMessage, tabsListContainer);
+    } else {
+        hideNoTabsMessage(noTabsMessage);
+        displayTabsList(tabsListContainer, response);
     }
-    else {
-        // Display the list
-        noTabsMessage.style.display = 'none';
-
-        const titleRow = document.createElement('li');
-        titleRow.classList.add('tabs-list-title', 'tabs-list-grid-format');
-
-        const activeTitle = document.createElement('label');
-        activeTitle.textContent = 'Active Page';
-        activeTitle.classList.add('tabs-title-columns')
-        titleRow.appendChild(activeTitle);
-
-        const timeTitle = document.createElement('label');
-        timeTitle.textContent = 'Time';
-        timeTitle.classList.add('tabs-title-columns')
-        titleRow.appendChild(timeTitle);
-
-        const xTitle = document.createElement('label');
-        xTitle.textContent = 'X';
-        xTitle.classList.add('tabs-title-columns');
-        xTitle.style.paddingLeft = '3px';
-        titleRow.appendChild(xTitle);
-
-        tabsListContainer.appendChild(titleRow);
-
-        for (const [ domain, data] of Object.entries(response)) {
-            const listItem = document.createElement('li');
-            listItem.classList.add('tabs-list-grid-format');
-
-            const headerContainer = document.createElement('div')
-            const header = document.createElement('h3');
-            header.style.paddingLeft = '10px'
-            header.textContent = domain;
-
-            headerContainer.classList.add('container')
-            headerContainer.appendChild(header);
-
-            const paragraphContainer = document.createElement('div')
-            const paragraph = document.createElement('p');
-            paragraph.style.fontSize = '1.2em';
-            paragraph.style.fontStyle = 'italic';
-            paragraph.style.paddingLeft = '10px'
-            paragraph.textContent = `${formatTime(data.totalTime)}`;
-
-            paragraphContainer.classList.add('container')
-            paragraphContainer.appendChild(paragraph)
-
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'X';
-            deleteButton.classList.add('delete-button');
-
-            deleteButton.addEventListener('click', () => {
-                deleteDomain(domain)
-            });
-
-            listItem.appendChild(headerContainer);
-            listItem.appendChild(paragraphContainer);
-            listItem.appendChild(deleteButton);
-
-            tabsListContainer.appendChild(listItem);
-        }
-    }
-
 }
 
-/**
- * Format the given time in seconds to a human-readable string.
- * @param {number} seconds - The time in seconds to be formatted.
- * @returns {string} - The formatted time string (e.g., "1h 30m 15s").
- */
+function displayNoTabsMessage(noTabsMessage, tabsListContainer) {
+    noTabsMessage.style.display = 'block';
+    tabsListContainer.style.display = 'none';
+}
+
+function hideNoTabsMessage(noTabsMessage) {
+    noTabsMessage.style.display = 'none';
+}
+
+function displayTabsList(tabsListContainer, response) {
+    const titleRow = createTitleRow();
+    tabsListContainer.appendChild(titleRow);
+
+    for (const [domain, data] of Object.entries(response)) {
+        const listItem = createListItem(domain, data);
+        tabsListContainer.appendChild(listItem);
+    }
+}
+
+function createTitleRow() {
+    const titleRow = document.createElement('li');
+    titleRow.classList.add('tabs-list-title', 'tabs-list-grid-format');
+
+    ['Active Page', 'Time', '/'].forEach(titleText => {
+        const title = createLabel(titleText, 'tabs-title-columns');
+        titleRow.appendChild(title);
+    });
+
+    return titleRow;
+}
+
+function createLabel(text, className) {
+    const label = document.createElement('label');
+    label.textContent = text;
+    label.classList.add(className);
+    return label;
+}
+
+function createListItem(domain, data) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('tabs-list-grid-format');
+
+    const headerContainer = createContainer();
+    headerContainer.appendChild(createHeader(domain));
+
+    const paragraphContainer = createContainer();
+    paragraphContainer.appendChild(createParagraph(formatTime(data.totalTime)));
+
+    const deleteButton = createDeleteButton(() => deleteDomain(domain));
+
+    listItem.appendChild(headerContainer);
+    listItem.appendChild(paragraphContainer);
+    listItem.appendChild(deleteButton);
+
+    return listItem;
+}
+
+function createContainer() {
+    const container = document.createElement('div');
+    container.classList.add('container');
+    return container;
+}
+
+function createHeader(text) {
+    const header = document.createElement('h3');
+    header.style.paddingLeft = '10px';
+    header.textContent = text;
+    return header;
+}
+
+function createParagraph(text) {
+    const paragraph = document.createElement('p');
+    paragraph.style.fontSize = '1.2em';
+    paragraph.style.fontStyle = 'italic';
+    paragraph.style.paddingLeft = '10px';
+    paragraph.textContent = text;
+    return paragraph;
+}
+
+function createDeleteButton(clickHandler) {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', clickHandler);
+    return deleteButton;
+}
+
 function formatTime(seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
@@ -96,11 +115,6 @@ function formatTime(seconds) {
     return `${hours}h ${minutes}m ${remainingSeconds}s`;
 }
 
-/**
- * Make request to backend script to delete tab data for domain.
- * @param {string} domain - the domain to be deleted.
- * @returns void.
- */
 function deleteDomain(domain) {
     chrome.runtime.sendMessage({ action: 'deleteDomain', domain }, () => {});
 }
